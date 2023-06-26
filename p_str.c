33cb9517dda1255c084cdaa459da63a4f22e3847
yaweffah03 <mr.effah@icloud.com>
2023-06-25 17:38:39 -0700

    updat

--- p_str.c
+++ p_str.c
@@ -1,10 +1,12 @@
 #include "monty.h"
+
 /**
- * f_pstr - prints the string starting at the top of the stack,
- * followed by a new
- * @head: stack head
- * @counter: line_number
- * Return: no return
+* f_pstr - prints the string starting at the top of the stack,
+* followed by a new
+* @head: head of stack
+* @counter: line count
+*
+* Return: nothing
 */
 void f_pstr(stack_t **head, unsigned int counter)
 {
--- queue.c
+++ queue.c
@@ -13,7 +13,7 @@ void f_queue(stack_t **head, unsigned int counter)
 }
 
 /**
- * addqueue - this add node to the tail stack
+ * addqueue - add node to the tail stack
  * @n: new_value
  * @head: head of the stack
  * Return: no return
